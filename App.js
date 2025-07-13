ffbcc35c8aed68281e5f3d4617ae85eedfd6a20b
Recoba82 <104107057+Recoba82@users.noreply.github.com>
2025-07-13 04:53:16 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function App() {
+  return <div style={{ padding: '2rem' }}><h1>Configuratore 3D Padel</h1><p>Benvenuto nel configuratore 3D.</p></div>;
+}
